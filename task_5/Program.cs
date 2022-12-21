9a910c568e9c582c7fc4b18bf7772c1c2bb7443b
MariaKrutikova <shagulina.m@gmail.com>
2022-12-21 13:55:10 +0300

    not done

---
+++ task_5/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4 8 -> 2, 4, 6, 8
+Console.Write("введите число");
+int numberN = int.Parse(Console.ReadLine());
+int numberC = 1;
+
+while (numberC <= numberN)
+{ 
+    Console.Write(numberC+" ");
+numberC++;    
+}
